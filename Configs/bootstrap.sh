0fc80a3d4fa1de8cddb64e82fd8c2fcf6f797928
于金玉 <yujinyu@hust.edu.cn>
2018-12-04 11:01:20 +0800

    mr-jobhistory-daemon.sh start historyserver

--- Configs/bootstrap.sh
+++ Configs/bootstrap.sh
@@ -15,6 +15,7 @@ sed -i s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
