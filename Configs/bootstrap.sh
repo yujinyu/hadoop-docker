2c5a026a05558c9d1dd9c6d4757c464d441ee469
于金玉 <yujinyu@hust.edu.cn>
2018-12-04 11:08:46 +0800

    ...

--- Configs/bootstrap.sh
+++ Configs/bootstrap.sh
@@ -13,9 +13,9 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 sed -i s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
+$HADOOP_PREFIX/bin/hdfs namenode -format
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
-$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
